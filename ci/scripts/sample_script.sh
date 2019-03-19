68b2b10a30165f1900d093ab729b0197a4cab493
Jim Munro <jim.munro@code42.com>
2019-03-19 11:18:55 -0600

    src

--- ci/scripts/sample_script.sh
+++ ci/scripts/sample_script.sh
@@ -3,4 +3,4 @@
 # python /source-code/src/example.py
 echo ">>>>>>>>>>>"
 # BASEDIR="$(cd "$(dirname "${BASH_SOURCE}")"/.. && pwd -P)"
-echo $(ls $BASH_SOURCE/src/example.py)
+echo $(ls $BASH_SOURCE/)
